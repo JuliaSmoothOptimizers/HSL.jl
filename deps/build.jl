8318d1f7c7de84f5e90aa4498bc7a15aa62a9ef7
Dominique Orban <dominique.orban@gmail.com>
2016-09-21 20:02:49 -0400

    explicitly add metis4 because Homebrew.jl will not do it

--- deps/build.jl
+++ deps/build.jl
@@ -17,6 +17,7 @@ noall_load = "--no-whole-archive"
   all_load = "-all_load"
   noall_load = "-noall_load"
   using Homebrew
+  Homebrew.add("homebrew/science/metis4")
   provides(Homebrew.HB, "homebrew/science/metis4", libmetis4, os=:Darwin)
   push!(Libdl.DL_LOAD_PATH, joinpath(Homebrew.prefix("metis4"), "lib"))
 end
