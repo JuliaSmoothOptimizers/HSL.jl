6f3207742cd284021b932656818996535d132876
Dominique Orban <dominique.orban@gmail.com>
2016-11-02 11:33:05 -0400

    test all data types

--- test/test_ma97.jl
+++ test/test_ma97.jl
@@ -1,93 +1,96 @@
-n = 5
-A = 3 * sprand(n, n, .5)
-A = A + A' + speye(n)
+for T in (Float32, Float64, Complex64, Complex128)
 
-# Test symmetric indefinite A.
-ma97 = Ma97(A)
-ma97_factorize(ma97)
-@assert ma97.info.flag == 0
+  println("testing with $T data")
+  matrix_type = T in (Complex64, Complex128) ? :herm_indef : :real_indef
 
-# Solve with one rhs.
-b = rand(n)
-x = ma97_solve(ma97, b)
-@assert ma97.info.flag == 0
-x_exact = A \ b
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  ϵ = sqrt(eps(real(T)))
+  n = 5
+  if VERSION >= v"0.5"
+    A = convert(T, 3) * sprand(T, n, n, .5)
+  else
+    A = convert(T, 3) * convert(SparseMatrixCSC{T,Int}, sprand(n, n, .5))
+  end
 
-# Solve with multiple rhs.
-b = rand(n, 3)
-x = ma97_solve(ma97, b)
-@assert ma97.info.flag == 0
-x_exact = A \ b
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  A = A + A' + speye(T, n)
 
-piv_order, d = ma97_inquire(ma97)
-@assert ma97.info.flag == 0
+  # Test symmetric indefinite A.
+  ma97 = Ma97(A)
+  ma97_factorize(ma97, matrix_type=matrix_type)
+  @test ma97.info.flag == 0
 
-# Test backslash.
-x = ma97 \ b
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  # Solve with one rhs.
+  b = rand(T, n)
+  x = ma97_solve(ma97, b)
+  @test ma97.info.flag == 0
+  x_exact = A \ b
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
 
-# Alter d.
-d[1,1] *= 2
-ma97_alter(ma97, d)
+  # Solve with multiple rhs.
+  b = rand(T, n, 3)
+  x = ma97_solve(ma97, b)
+  @test ma97.info.flag == 0
+  x_exact = A \ b
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
 
-# Test convenience interface.
-x = ma97_solve(A, b)
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  piv_order, d = ma97_inquire(ma97, matrix_type=matrix_type)
+  @test ma97.info.flag == 0
 
-# Test symmetric positive definite A.
-A = A * A';
-ma97 = Ma97(A)
-ma97_factorize(ma97, matrix_type=:real_spd)
-@assert ma97.info.flag == 0
+  # Test backslash.
+  x = ma97 \ b
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
 
-b = rand(n)
-x = ma97_solve(ma97, b)
-@assert ma97.info.flag == 0
-x_exact = A \ b
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  # Alter d.
+  d[1,1] *= 2
+  ma97_alter(ma97, d)
 
-# Solve with multiple rhs.
-b = rand(n, 3)
-x = ma97_solve(ma97, b)
-@assert ma97.info.flag == 0
-x_exact = A \ b
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  # Test convenience interface.
+  x = ma97_solve(A, b, matrix_type=matrix_type)
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
 
-d = ma97_inquire(ma97, matrix_type=:real_spd)
-@assert ma97.info.flag == 0
+  # Test symmetric positive definite A.
+  A = A * A';
+  matrix_type = T in (Complex64, Complex128) ? :herm_pd : :real_spd
 
-# Test rectangular A.
-A = rand(10, 6)
-b = rand(10)
-(r, x) = ma97_least_squares(A, b)
-x_exact = A \ b
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  ma97 = Ma97(A)
+  ma97_factorize(ma97, matrix_type=matrix_type)
+  @test ma97.info.flag == 0
 
-A = rand(6, 10)
-b = rand(6)
-(x, y) = ma97_min_norm(A, b)
-x_exact = A \ b
-@assert norm(x - x_exact) ≤ 1.0e-8 * norm(x_exact)
+  b = rand(T, n)
+  x = ma97_solve(ma97, b)
+  @test ma97.info.flag == 0
+  x_exact = A \ b
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
 
-# Test if errors are thrown correctly.
-A = randn(10, 5)
-try
-  Ma97(A)
-catch E
-  @assert isa(E,Ma97Exception)
-end
+  # Solve with multiple rhs.
+  b = rand(T, n, 3)
+  x = ma97_solve(ma97, b)
+  @test ma97.info.flag == 0
+  x_exact = A \ b
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
 
-A = rand(6, 10)
-b = rand(3)
-try
+  d = ma97_inquire(ma97, matrix_type=matrix_type)
+  @test ma97.info.flag == 0
+
+  # Test rectangular A.
+  A = rand(T, 10, 6)
+  b = rand(T, 10)
+  (r, x) = ma97_least_squares(A, b)
+  x_exact = A \ b
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
+
+  A = rand(T, 6, 10)
+  b = rand(T, 6)
   (x, y) = ma97_min_norm(A, b)
-catch E
-  @assert isa(E,Ma97Exception)
-end
-try
-   ma97_solve(ma97, b)
-catch E
-  @assert isa(E,Ma97Exception)
+  x_exact = A \ b
+  @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
+
+  # Test if errors are thrown correctly.
+  A = rand(T, 10, 5)
+  @test_throws Ma97Exception Ma97(A)
+
+  A = rand(T, 6, 10)
+  b = rand(T, 3)
+  @test_throws Ma97Exception ma97_min_norm(A, b)
+  @test_throws Ma97Exception ma97_solve(ma97, b)
+
 end
