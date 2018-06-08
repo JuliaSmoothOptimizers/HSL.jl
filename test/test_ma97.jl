c7a422b4e6775c309e8fd847a0ff7873683f10a9
Dominique Orban <dominique.orban@gmail.com>
2018-06-07 22:47:07 -0700

    add info about expected warnings

--- test/test_ma97.jl
+++ test/test_ma97.jl
@@ -76,12 +76,14 @@ for T in (Float32, Float64, Complex64, Complex128)
   # Test rectangular A.
   A = rand(T, 10, 6)
   b = rand(T, 10)
+  info("Warning below is expected and normal")
   (r, x) = ma97_least_squares(A, b)
   x_exact = A \ b
   @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
 
   A = rand(T, 6, 10)
   b = rand(T, 6)
+  info("Warning below is expected and normal")
   (x, y) = ma97_min_norm(A, b)
   x_exact = A \ b
   @test norm(x - x_exact) ≤ ϵ * norm(x_exact)
