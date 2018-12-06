f329a6773b820e0f4313f29912cc31637dd6d1d0
Dominique Orban <dominique.orban@gmail.com>
2018-12-05 23:49:06 -0500

    slightly more robust detection of dependencies

--- deps/build_blas.jl
+++ deps/build_blas.jl
@@ -10,3 +10,14 @@ end
 
 provides(AptGet, "libblas-dev", libblas, os=:Linux)
 provides(AptGet, "liblapack-dev", liblapack, os=:Linux)
+
+# try to guess what BinDeps installed
+blas_lib = "-lblas"
+lapack_lib = "-llapack"
+@static if Sys.isapple()
+  hb_libopenblas_dir = joinpath(Homebrew.prefix("openblas"), "lib")
+  if isdir(hb_libopenblas_dir)
+    blas_lib = "-L$hb_libopenblas_dir -lopenblas"
+    lapack_lib = "-L$hb_libopenblas_dir -lopenblas"
+  end
+end
--- deps/build_hsl_ma97.jl
+++ deps/build_hsl_ma97.jl
@@ -2,6 +2,7 @@
 
 # Users should place hsl_ma97-x.y.z.tar.gz in ~/.julia/v0.j/HSL/deps/downloads
 libhsl_ma97 = library_dependency("libhsl_ma97", depends=[libblas, liblapack, libmetis4])
+# libhsl_ma97 = library_dependency("libhsl_ma97", depends=[libopenblas, libmetis4])
 
 hsl_ma97_depsdir = BinDeps.depsdir(libhsl_ma97)
 hsl_ma97_srcdir = joinpath(hsl_ma97_depsdir, "src")
@@ -16,9 +17,9 @@ provides(SimpleBuild,
             FileUnpacker(hsl_ma97_archive, hsl_ma97_srcdir, "hsl_ma97-$hsl_ma97_version")
             (@build_steps begin
               ChangeDirectory(hsl_ma97_src)
-              `./configure F77=gfortran CFLAGS=-fPIC FFLAGS="-fPIC -fopenmp" FCFLAGS="-fPIC -fopenmp" --prefix=$hsl_ma97_prefix --with-blas=-lblas --with-lapack=-llapack --with-metis="-L$metis_libpath -lmetis"`
+              `./configure F77=gfortran CFLAGS=-fPIC FFLAGS="-fPIC -fopenmp" FCFLAGS="-fPIC -fopenmp" --prefix=$hsl_ma97_prefix --with-blas="$blas_lib" --with-lapack="$lapack_lib" --with-metis="$metis_libpath"`
               `make install`
-              `gfortran -fPIC -shared -Wl,$all_load $hsl_ma97_libdir/libhsl_ma97.a -lblas -llapack -L$metis_libpath -lmetis -lgomp -Wl,$noall_load -o $hsl_ma97_libdir/libhsl_ma97.$so`
+              `gfortran -fPIC -shared -Wl,$all_load $hsl_ma97_libdir/libhsl_ma97.a $(split(blas_lib)) $(split(lapack_lib)) $(split(metis_libpath)) -lgomp -Wl,$noall_load -o $hsl_ma97_libdir/libhsl_ma97.$so`
             end)
           end), libhsl_ma97, os=:Unix)
 
