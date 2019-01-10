2d79bd94150ba1ea86676d6c7d0d0d5ed987afc7
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-10 03:14:53 +0000

    build based on 769dc56

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "reference/#",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "reference/#Reference-1",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "section",
+    "text": "HSL MA57 and MA97 can be used for the solution of symmetric, possibly indefinite, linear systems. They are often used for the solution of saddle-point systems, i.e., systems of the formbeginbmatrix\nH  A^T \nA \nendbmatrix\nbeginbmatrix\nx  y\nendbmatrix\n=\nbeginbmatrix\nb  c\nendbmatrixSpecial cases occur when H is the identity matrix and either b = 0 or c = 0, which correspond to the least-squares problemmin_y  A^T y - band to the least-norm problemmin_x  x  textsubject to  Ax = crespectively.Modules = [HSL]\nPrivate = false\nOrder   = [:module, :constant, :type, :function, :macro]"
+},
+
+]}
