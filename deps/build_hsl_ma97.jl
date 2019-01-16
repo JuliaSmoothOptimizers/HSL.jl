8c22e1c342d1eef4226576a8da2ec3f890e4971c
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-01-15 20:53:42 -0500

    Change from BinDeps to BinaryProvider

--- deps/build_hsl_ma97.jl
+++ deps/build_hsl_ma97.jl
@@ -1,26 +1,13 @@
-# HSL_MA97 package-specific dependencies.
-
 # Users should place hsl_ma97-x.y.z.tar.gz in ~/.julia/v0.j/HSL/deps/downloads
-libhsl_ma97 = library_dependency("libhsl_ma97", depends=[libblas, liblapack, libmetis4])
-# libhsl_ma97 = library_dependency("libhsl_ma97", depends=[libopenblas, libmetis4])
 
-hsl_ma97_depsdir = BinDeps.depsdir(libhsl_ma97)
-hsl_ma97_srcdir = joinpath(hsl_ma97_depsdir, "src")
-hsl_ma97_src = joinpath(hsl_ma97_srcdir, "hsl_ma97-$hsl_ma97_version")
-hsl_ma97_prefix = joinpath(hsl_ma97_depsdir, "usr")
-hsl_ma97_libdir = joinpath(hsl_ma97_depsdir, "usr", "lib")
-
-provides(SimpleBuild,
-         (@build_steps begin
-            BinDeps.ChecksumValidator(hsl_ma97_sha256, hsl_ma97_archive)
-            CreateDirectory(hsl_ma97_srcdir)
-            FileUnpacker(hsl_ma97_archive, hsl_ma97_srcdir, "hsl_ma97-$hsl_ma97_version")
-            (@build_steps begin
-              ChangeDirectory(hsl_ma97_src)
-              `./configure F77=gfortran CFLAGS=-fPIC FFLAGS="-fPIC -fopenmp" FCFLAGS="-fPIC -fopenmp" --prefix=$hsl_ma97_prefix --with-blas="$blas_lib" --with-lapack="$lapack_lib" --with-metis="$metis_libpath"`
-              `make install`
-              `gfortran -fPIC -shared -Wl,$all_load $hsl_ma97_libdir/libhsl_ma97.a $(split(blas_lib)) $(split(lapack_lib)) $(split(metis_libpath)) -lgomp -Wl,$noall_load -o $hsl_ma97_libdir/libhsl_ma97.$so`
-            end)
-          end), libhsl_ma97, os=:Unix)
-
-hsl_modules[:libhsl_ma97] = :libhsl_ma97
+open(hsl_ma97_archive) do f
+  if bytes2hex(sha256(f)) != hsl_ma97_sha256
+    error("SHA256 of HSL MA97 doesn't match")
+  end
+end
+run(`tar -zxf $hsl_ma97_archive -C $builddir`)
+cd("$builddir/hsl_ma97-$hsl_ma97_version")
+run(`./configure --prefix=$usrdir F77=gfortran CFLAGS=-fPIC FFLAGS="-fPIC -fopenmp" FCFLAGS="-fPIC -fopenmp" --with-blas="-L$libdir -lopenblas" --with-metis="-L$libdir -lcoinmetis"`)
+run(`make install`)
+run(`gfortran -fPIC -shared -Wl,$all_load $libdir/libhsl_ma97.a -L$libdir -lopenblas -lcoinmetis -lgomp -Wl,$noall_load -o $libdir/libhsl_ma97.$so`)
+cd(@__DIR__)
