0a160d95f3a716e2d4cf0eaa97e214e187f4bc5d
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-04-30 16:09:34 -0300

    Build MA57 even without get_factors.patch
    If get_factors.patch is included, then src/hsl_57_patch.jl is included, which includes functions
    ma57_get_factors and ma57_alter_d.

--- deps/build_hsl_ma57.jl
+++ deps/build_hsl_ma57.jl
@@ -7,8 +7,9 @@ open(hsl_ma57_archive) do f
 end
 run(`tar -zxf $hsl_ma57_archive -C $builddir`)
 cd("$builddir/hsl_ma57-$hsl_ma57_version")
-patchfile = joinpath(@__DIR__, "downloads", "get_factors.patch")
-run(`patch -p1 -i $patchfile`)
+if isfile(hsl_ma57_patch)
+  run(`patch -p1 -i $hsl_ma57_patch`)
+end
 run(`./configure --prefix=$usrdir F77=gfortran CFLAGS=-fPIC FFLAGS="-fPIC -fopenmp" FCFLAGS="-fPIC -fopenmp" --with-blas="-L$libdir -lopenblas" --with-metis="-L$libdir -lcoinmetis"`)
 run(`make install`)
 run(`gfortran -fPIC -shared -Wl,$all_load $libdir/libhsl_ma57.a -L$libdir -lopenblas -lcoinmetis -lgomp -Wl,$noall_load -o $libdir/libhsl_ma57.$so`)
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,9 @@ include("../deps/deps.jl")
 
 if @isdefined libhsl_ma57
   include("test_ma57.jl")
+  if @isdefined libhsl_ma57_patch
+    include("test_ma57_patch.jl")
+  end
 end
 if @isdefined libhsl_ma97
   include("test_ma97.jl")
