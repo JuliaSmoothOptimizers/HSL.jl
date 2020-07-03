05819ceb14da316d76bb53893867355fd31740d4
Dominique Orban <dominique.orban@gmail.com>
2020-07-03 15:52:41 -0400

    interface for non-default control parameters

--- test/test_ma57.jl
+++ test/test_ma57.jl
@@ -1,6 +1,6 @@
 import SparseArrays.getindex_traverse_col
 # this should be fixed in a future release
-# see 
+# see
 getindex_traverse_col(::AbstractUnitRange, lo::Integer, hi::Integer) = lo:hi
 
 function test_ma57(A, M, b, xexact)
@@ -30,18 +30,30 @@ end
 for T in (Float32, Float64)
 
   @info "Testing hsl_ma57 with $T data"
+  n = 5
   rows = Int32[1, 1, 2, 2, 3, 3, 5]
   cols = Int32[1, 2, 3, 5, 3, 4, 5]
   vals = T[2, 3, 4, 6, 1, 5, 1]
-  A = sparse(rows, cols, vals) ; A = A + triu(A, 1)'
+  A = sparse(rows, cols, vals, n, n) ; A = A + triu(A, 1)'
 
-  M = ma57_coord(5, rows, cols, vals)
+  M = ma57_coord(n, rows, cols, vals)
 
   b = T[8, 45, 31, 15, 17]
   xexact = T[1, 2, 3, 4, 5]
   test_ma57(A, M, b, xexact)
 
-  n = 5
+  # test with non-default options
+  control = Ma57_Control{T}()
+  info = Ma57_Info{T}()
+  control.icntl[6] = 4  # choose a different ordering
+  # give lower triangle: swap rows and cols
+  LBL = ma57_coord(n, cols, rows, vals, control, info)
+  ma57_factorize(LBL)
+  x = copy(b)
+  ma57_solve!(LBL, x)
+  ϵ = sqrt(eps(T))
+  @test norm(x - xexact) ≤ ϵ * norm(xexact)
+
   A = convert(T, 3) * convert(SparseMatrixCSC{T,Int32}, sprand(T, n, n, .5))
   A = A + A' + convert(SparseMatrixCSC{T,Int32}, sparse(T(1)*I, n, n))
   M = Ma57(A)
