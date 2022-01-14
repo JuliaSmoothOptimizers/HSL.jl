b7e6451ceb303638e73cc460e185bed1d8a7604d
Brian Jackson <bjack205@gmail.com>
2022-01-13 23:37:59 -0500

    Don't include deps.jl in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,14 @@ using LinearAlgebra
 using SparseArrays
 using Test
 
-include("../deps/deps.jl")
+# include("../deps/deps.jl")
 
-if @isdefined libhsl_ma57
+if HSL.haslib(:hsl_ma57) 
   include("test_ma57.jl")
-  if @isdefined libhsl_ma57_patch
+  if isdefined(HSL, :libhsl_ma57_patch)
     include("test_ma57_patch.jl")
   end
 end
-if @isdefined libhsl_ma97
+if HSL.haslib(:hsl_ma97)
   include("test_ma97.jl")
 end
