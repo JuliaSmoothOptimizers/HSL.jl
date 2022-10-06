350824ff9d72ea719b9c0d1d2aa49674ae63d61b
dpo <dpo@users.noreply.github.com>
2022-10-06 03:21:13 +0000

    :robot: Format .jl files

--- deps/versions.jl
+++ deps/versions.jl
@@ -14,11 +14,7 @@ hsl_collection["hsl_ma97"] = [
     "ac3a081d3a28e9ecb8871ce769f4ced2a5ffa5a9c36defbd2c844ae3493ccb37",
     ".tar.gz",
   ),
-  HSLVersion(
-    "2.7.0",
-    "8221b607d96554d7a57cc60483c7305ef43a8785dc4171ac2e8da087900a1100",
-    ".zip",
-  ),
+  HSLVersion("2.7.0", "8221b607d96554d7a57cc60483c7305ef43a8785dc4171ac2e8da087900a1100", ".zip"),
   HSLVersion(
     "2.6.0",
     "be5fe822674be93e3d2e1a7d7ed6c5ad831b91cf8ca5150beb473f67af5fcb66",
@@ -32,9 +28,5 @@ hsl_collection["mc21"] = [
     "9d2a509a35e7826f564ff637098e7d0e205a87cb99826ec1ee9c1e9b21c6d71a",
     ".tar.gz",
   ),
-  HSLVersion(
-    "1.0.0",
-    "fc5b2cdced0486a18ae1ee0ea375040d779fe17cd69db9c5c8819b50dd962c63",
-    ".zip",
-  ),
+  HSLVersion("1.0.0", "fc5b2cdced0486a18ae1ee0ea375040d779fe17cd69db9c5c8819b50dd962c63", ".zip"),
 ]
--- test/test_mc21.jl
+++ test/test_mc21.jl
@@ -1,8 +1,10 @@
 @testset "mc21" begin
-  A = [1  0 0  4;
-       0  0 7  8;
-       9  0 0 12;
-       0 14 0 16]
+  A = [
+    1 0 0 4
+    0 0 7 8
+    9 0 0 12
+    0 14 0 16
+  ]
   Aᴴ = sparse(A')
   iperm = mc21(Aᴴ)
   @test iperm == Int32[1; 4; 2; 3]
@@ -10,12 +12,12 @@
   n = 100
   A = rand(n, n)
   for i = 1:n
-      A[i,i] = 0
+    A[i, i] = 0
   end
   A = SparseMatrixCSC(A)
   p = mc21(A)
   @test minimum(p) == 1
   @test maximum(p) == n
   @test isperm(p)
-  @test 0 ∉ diag(A[:,p])
+  @test 0 ∉ diag(A[:, p])
 end
