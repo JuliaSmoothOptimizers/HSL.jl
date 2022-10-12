95be76ef1488e95ef144a3f13c24de2c88262aeb
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-10-12 19:43:41 -0400

    Fix issues with global variables

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,18 @@ using LinearAlgebra
 using SparseArrays
 using Test
 
-if (@isdefined HSL.available_hsl_algorithms)
-  for (software, version) in HSL.available_hsl_algorithms
+if isfile(joinpath(@__DIR__, "..", "deps", "deps.jl"))
+  include("../deps/deps.jl")
+end
+
+if @isdefined available_hsl_algorithms
+  for (software, version) in available_hsl_algorithms
     @info("$software: v\"$version\"")
   end
 
-  for package in keys(HSL.available_hsl_algorithms)
+  for package in keys(available_hsl_algorithms)
     include("test_$(package).jl")
-    if package == "hsl_ma57" && HSL.hsl_ma57_patched
+    if package == "hsl_ma57" && hsl_ma57_patched
       include("test_hsl_ma57_patch.jl")
     end
   end
