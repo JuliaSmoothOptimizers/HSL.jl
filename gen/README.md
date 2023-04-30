0f33c9e164f7b617b5488f2a64fc367aeeac91dc
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-04-30 16:24:30 -0400

    Add wrappers for HSL packages with a C interface

--- gen/README.md
+++ gen/README.md
@@ -1,16 +1,17 @@
 # Wrapping headers
 
-This directory contains scripts that can be used to automatically generate wrappers for C headers by HSL libraries.
+This directory contains a script that can be used to automatically generate wrappers from C headers provided by the HSL libraries.
 This is done using Clang.jl.
 
 # Usage
 
 Either run `julia wrapper.jl` directly, or include it and call the `main()` function.
-Be sure to activate the project environment in this folder, which will install `Clang.jl` and `JuliaFormatter.jl`.
+Be sure to activate the project environment in this folder (`julia --project`), which will install `Clang.jl` and `JuliaFormatter.jl`.
 The `main` function supports the boolean keyword argument `optimized` to clear the generated wrappers.
 You can also call `main(library)` if you want to generate the wrapper for a specific HSL `library`.
 The possible values for `library` are:
 - `"all"` (default);
+- `"juliahsl"`;
 - `"hsl_ma48"`;
 - `"hsl_ma57"`;
 - `"hsl_ma77"`;
@@ -22,4 +23,9 @@ The possible values for `library` are:
 - `"hsl_mc69"`;
 - `"hsl_mc78"`;
 - `"hsl_mc79"`;
-- `"hsl_mi20"`.
+- `"hsl_mi20"`;
+- `"hsl_mi28"`.
+
+# Maintenance
+
+If a new C interface is available, add an entry for it in the `main` function of `wrapper.jl`.
---
+++ src/C/hsl_ma86.jl
@@ -0,0 +1,192 @@
+mutable struct ma86_control{T}
+    f_arrays::Cint
+    diagnostics_level::Cint
+    unit_diagnostics::Cint
+    unit_error::Cint
+    unit_warning::Cint
+    nemin::Cint
+    nb::Cint
+    action::Cint
+    nbi::Cint
+    pool_size::Cint
+    small_::T
+    static_::T
+    u::T
+    umin::T
+    scaling::Cint
+end
+
+function ma86_default_control_s(control)
+    @ccall libhsl.ma86_default_control_s(control::Ref{ma86_control{Float32}})::Cvoid
+end
+
+mutable struct ma86_info{T}
+    detlog::T
+    detsign::Cint
+    flag::Cint
+    matrix_rank::Cint
+    maxdepth::Cint
+    num_delay::Cint
+    num_factor::Clong
+    num_flops::Clong
+    num_neg::Cint
+    num_nodes::Cint
+    num_nothresh::Cint
+    num_perturbed::Cint
+    num_two::Cint
+    pool_size::Cint
+    stat::Cint
+    usmall::T
+end
+
+function ma86_analyse_s(n, ptr, row, order, keep, control, info)
+    @ccall libhsl.ma86_analyse_s(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint}, order::Ptr{Cint},
+                                 keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float32}},
+                                 info::Ref{ma86_info{Float32}})::Cvoid
+end
+
+function ma86_factor_s(n, ptr, row, val, order, keep, control, info, scale)
+    @ccall libhsl.ma86_factor_s(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint},
+                                val::Ptr{Float32}, order::Ptr{Cint},
+                                keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float32}},
+                                info::Ref{ma86_info{Float32}}, scale::Ptr{Float32})::Cvoid
+end
+
+function ma86_factor_solve_s(n, ptr, row, val, order, keep, control, info, nrhs, ldx, x,
+                             scale)
+    @ccall libhsl.ma86_factor_solve_s(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint},
+                                      val::Ptr{Float32}, order::Ptr{Cint},
+                                      keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float32}},
+                                      info::Ref{ma86_info{Float32}}, nrhs::Cint, ldx::Cint,
+                                      x::Ptr{Float32},
+                                      scale::Ptr{Float32})::Cvoid
+end
+
+function ma86_solve_s(job, nrhs, ldx, x, order, keep, control, info, scale)
+    @ccall libhsl.ma86_solve_s(job::Cint, nrhs::Cint, ldx::Cint, x::Ptr{Float32},
+                               order::Ptr{Cint}, keep::Ptr{Ptr{Cvoid}},
+                               control::Ref{ma86_control{Float32}}, info::Ref{ma86_info{Float32}},
+                               scale::Ptr{Float32})::Cvoid
+end
+
+function ma86_finalise_s(keep, control)
+    @ccall libhsl.ma86_finalise_s(keep::Ptr{Ptr{Cvoid}},
+                                  control::Ref{ma86_control{Float32}})::Cvoid
+end
+
+function ma86_default_control_d(control)
+    @ccall libhsl.ma86_default_control_d(control::Ref{ma86_control{Float64}})::Cvoid
+end
+
+function ma86_analyse_d(n, ptr, row, order, keep, control, info)
+    @ccall libhsl.ma86_analyse_d(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint}, order::Ptr{Cint},
+                                 keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float64}},
+                                 info::Ref{ma86_info{Float64}})::Cvoid
+end
+
+function ma86_factor_d(n, ptr, row, val, order, keep, control, info, scale)
+    @ccall libhsl.ma86_factor_d(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint},
+                                val::Ptr{Float64}, order::Ptr{Cint},
+                                keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float64}},
+                                info::Ref{ma86_info{Float64}}, scale::Ptr{Float64})::Cvoid
+end
+
+function ma86_factor_solve_d(n, ptr, row, val, order, keep, control, info, nrhs, ldx, x,
+                             scale)
+    @ccall libhsl.ma86_factor_solve_d(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint},
+                                      val::Ptr{Float64}, order::Ptr{Cint},
+                                      keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float64}},
+                                      info::Ref{ma86_info{Float64}}, nrhs::Cint, ldx::Cint,
+                                      x::Ptr{Float64},
+                                      scale::Ptr{Float64})::Cvoid
+end
+
+function ma86_solve_d(job, nrhs, ldx, x, order, keep, control, info, scale)
+    @ccall libhsl.ma86_solve_d(job::Cint, nrhs::Cint, ldx::Cint, x::Ptr{Float64},
+                               order::Ptr{Cint}, keep::Ptr{Ptr{Cvoid}},
+                               control::Ref{ma86_control{Float64}}, info::Ref{ma86_info{Float64}},
+                               scale::Ptr{Float64})::Cvoid
+end
+
+function ma86_finalise_d(keep, control)
+    @ccall libhsl.ma86_finalise_d(keep::Ptr{Ptr{Cvoid}},
+                                  control::Ref{ma86_control{Float64}})::Cvoid
+end
+
+function ma86_default_control_c(control)
+    @ccall libhsl.ma86_default_control_c(control::Ref{ma86_control{Float32}})::Cvoid
+end
+
+function ma86_analyse_c(n, ptr, row, order, keep, control, info)
+    @ccall libhsl.ma86_analyse_c(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint}, order::Ptr{Cint},
+                                 keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float32}},
+                                 info::Ref{ma86_info{Float32}})::Cvoid
+end
+
+function ma86_factor_c(matrix_type, n, ptr, row, val, order, keep, control, info, scale)
+    @ccall libhsl.ma86_factor_c(matrix_type::Cint, n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint},
+                                val::Ptr{ComplexF32}, order::Ptr{Cint},
+                                keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float32}},
+                                info::Ref{ma86_info{Float32}}, scale::Ptr{Float32})::Cvoid
+end
+
+function ma86_factor_solve_c(matrix_type, n, ptr, row, val, order, keep, control, info,
+                             nrhs, ldx, x, scale)
+    @ccall libhsl.ma86_factor_solve_c(matrix_type::Cint, n::Cint, ptr::Ptr{Cint},
+                                      row::Ptr{Cint}, val::Ptr{ComplexF32},
+                                      order::Ptr{Cint}, keep::Ptr{Ptr{Cvoid}},
+                                      control::Ref{ma86_control{Float32}}, info::Ref{ma86_info{Float32}},
+                                      nrhs::Cint, ldx::Cint, x::Ptr{ComplexF32},
+                                      scale::Ptr{Float32})::Cvoid
+end
+
+function ma86_solve_c(job, nrhs, ldx, x, order, keep, control, info, scale)
+    @ccall libhsl.ma86_solve_c(job::Cint, nrhs::Cint, ldx::Cint, x::Ptr{ComplexF32},
+                               order::Ptr{Cint}, keep::Ptr{Ptr{Cvoid}},
+                               control::Ref{ma86_control{Float32}}, info::Ref{ma86_info{Float32}},
+                               scale::Ptr{Float32})::Cvoid
+end
+
+function ma86_finalise_c(keep, control)
+    @ccall libhsl.ma86_finalise_c(keep::Ptr{Ptr{Cvoid}},
+                                  control::Ref{ma86_control{Float32}})::Cvoid
+end
+
+function ma86_default_control_z(control)
+    @ccall libhsl.ma86_default_control_z(control::Ref{ma86_control{Float64}})::Cvoid
+end
+
+function ma86_analyse_z(n, ptr, row, order, keep, control, info)
+    @ccall libhsl.ma86_analyse_z(n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint}, order::Ptr{Cint},
+                                 keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float64}},
+                                 info::Ref{ma86_info{Float64}})::Cvoid
+end
+
+function ma86_factor_z(matrix_type, n, ptr, row, val, order, keep, control, info, scale)
+    @ccall libhsl.ma86_factor_z(matrix_type::Cint, n::Cint, ptr::Ptr{Cint}, row::Ptr{Cint},
+                                val::Ptr{ComplexF64}, order::Ptr{Cint},
+                                keep::Ptr{Ptr{Cvoid}}, control::Ref{ma86_control{Float64}},
+                                info::Ref{ma86_info{Float64}}, scale::Ptr{Float64})::Cvoid
+end
+
+function ma86_factor_solve_z(matrix_type, n, ptr, row, val, order, keep, control, info,
+                             nrhs, ldx, x, scale)
+    @ccall libhsl.ma86_factor_solve_z(matrix_type::Cint, n::Cint, ptr::Ptr{Cint},
+                                      row::Ptr{Cint}, val::Ptr{ComplexF64},
+                                      order::Ptr{Cint}, keep::Ptr{Ptr{Cvoid}},
+                                      control::Ref{ma86_control{Float64}}, info::Ref{ma86_info{Float64}},
+                                      nrhs::Cint, ldx::Cint, x::Ptr{ComplexF64},
+                                      scale::Ptr{Float64})::Cvoid
+end
+
+function ma86_solve_z(job, nrhs, ldx, x, order, keep, control, info, scale)
+    @ccall libhsl.ma86_solve_z(job::Cint, nrhs::Cint, ldx::Cint, x::Ptr{ComplexF64},
+                               order::Ptr{Cint}, keep::Ptr{Ptr{Cvoid}},
+                               control::Ref{ma86_control{Float64}}, info::Ref{ma86_info{Float64}},
+                               scale::Ptr{Float64})::Cvoid
+end
+
+function ma86_finalise_z(keep, control)
+    @ccall libhsl.ma86_finalise_z(keep::Ptr{Ptr{Cvoid}},
+                                  control::Ref{ma86_control{Float64}})::Cvoid
+end
