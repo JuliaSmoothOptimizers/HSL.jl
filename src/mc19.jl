80f6c8436ec54d109be89a0cfc70d8ab87e0676f
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-05-08 01:16:02 -0500

    Interface MC19

---
+++ src/mc19.jl
@@ -0,0 +1,29 @@
+export mc19
+
+"""
+    r, c = mc19(A::SparseMatrixCSC)
+
+Compute diagonal scaling factors for the square sparse matrix `A` so that the nonzeros of the scaled
+matrix are as close to unity as possible in a least-squares logarithmic sense.
+"""
+function mc19 end
+
+function mc19(A::SparseMatrixCSC{T,INT}) where {T,INT}
+  m, n = size(A)
+  na = nnz(A)
+  irn, icn, a = findnz(A)
+  r = Vector{Float32}(undef, n)
+  c = Vector{Float32}(undef, n)
+  w = Vector{Float32}(undef, 5*n)
+  mc19ar(T, INT, n, na, a, irn, icn, r, c, w)
+  return r, c
+end
+
+function mc19(n::Integer, irn::Vector{INT}, icn::Vector{INT}, a::Vector{T}) where {T,INT}
+  na = length(a)
+  r = Vector{Float32}(undef, n)
+  c = Vector{Float32}(undef, n)
+  w = Vector{Float32}(undef, 5*n)
+  mc19ar(T, INT, n, na, a, irn, icn, r, c, w)
+  return r, c
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,7 @@ if LIBHSL_isfunctional()
   include("test_hsl_ma57.jl")
   include("test_hsl_ma97.jl")
   include("test_kb07.jl")
+  include("test_mc19.jl")
   include("test_mc21.jl")
   include("test_mc77.jl")
 else
